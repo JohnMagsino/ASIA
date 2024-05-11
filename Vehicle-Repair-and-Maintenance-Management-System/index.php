33b706b479a49e6f4bea40da13886f0f9fd3d9ec
super-smile <21-39754@g.batstate-u.edu.ph>
2024-05-11 21:10:49 +0800

    login

--- Vehicle-Repair-and-Maintenance-Management-System/index.php
+++ Vehicle-Repair-and-Maintenance-Management-System/index.php
@@ -47,41 +47,6 @@
 		
     </style>
 </head>
-<<<<<<< Updated upstream
-
-<body>
-	<div class="row">
-		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-			<div class="login-panel panel panel-default">
-				<center><img src="assets/image/login-logo.png"></center>
-				<div class="panel-body">
-					<form role="form">
-						<fieldset>
-							<div class="form-group">
-								<input class="form-control" placeholder="Username" type="text" autofocus="">
-							</div>
-							<div class="form-group">
-								<input class="form-control" placeholder="Password" type="password" value="">
-							</div>
-							<div class="checkbox">
-								<label>
-									<input name="remember" type="checkbox" value="Remember Me">Remember Me
-								</label>
-							</div>
-							<center><a href="admin" class="btn btn-success btn-lg">Login as Admin</a>
-								<a href="customer" class="btn btn-info btn-lg">Login as Customer</a>
-							</center>
-						</fieldset>
-					</form>
-				</div>
-			</div>
-		</div><!-- /.col-->
-	</div><!-- /.row -->
-
-
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-=======
 <body style="padding: 0">
 <div class="container-fluid" style="background: #f1f1f1;">
     <div class="row">
@@ -125,7 +90,6 @@
 
 <script src="assets/js/jquery-1.11.1.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
->>>>>>> Stashed changes
 </body>
 
 </html>
